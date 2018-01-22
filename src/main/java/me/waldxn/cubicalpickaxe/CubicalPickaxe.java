ff0b6758d707bd754e7a975c7e9b689f0139de48
Waldxn <chriswalden80@gmail.com>
2018-01-22 16:59:01 -0500

    Initial commit

---
+++ src/main/java/me/waldxn/cubicalpickaxe/CubicalPickaxe.java
@@ -0,0 +1,17 @@
+package me.waldxn.cubicalpickaxe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CubicalPickaxe extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
